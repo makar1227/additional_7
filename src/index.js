acbdf7d58056f16a88d1c6b00d4356218e526a12
makar1227 <makar1227@gmail.com>
2018-03-01 23:30:04 +0300

    res

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+module.exports =
+    function solve(matrix) {
+        let x = true;
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (matrix[i][j] == 0) {
+                    for (let k = 1; k < 10; k++) {
+                        if (checkLines(matrix, i, j, k) && checkSquere(matrix, i, j, k)) {
+                            matrix[i][j] = k;
+                            if (solve(matrix))
+                                return matrix;
+                            matrix[i][j] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+function checkLines(matrix, i, j, k) {
+    for (let a = 0; a < 3; a++) {
+        for (let b = 0; b < 3; b++)
+            if (a != i && b != j && matrix[(Math.floor((i / 3)) * 3) + a][(Math.floor((j / 3)) * 3) + b] == k)
+                return false;
+    }
+    return true;
+}
+
+function checkSquere(matrix, i, j, k) {
+    for (let a = 0; a < 9; a++) {
+        if ((a != i && matrix[a][j] == k) || (a != j && matrix[i][a] == k))
+            return false;
+    }
+        return true;
 }
